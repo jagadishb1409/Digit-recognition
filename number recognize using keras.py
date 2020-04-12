7c7f375e023bff7d569a029d109d869312b71977
jagadishb1409 <63513035+jagadishb1409@users.noreply.github.com>
2020-04-12 05:20:48 +0000

    Update number recognize using keras.py

--- number recognize using keras.py
+++ number recognize using keras.py
@@ -1,13 +1,17 @@
+# importing keras
 from tensorflow import keras
 
+# dividing MNIST datasets to training and test datasets
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+# converting dataset to float32 type
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
+# preparing the model
 model = keras.models.Sequential([
     keras.layers.Conv2D(64, (2, 2), padding='same',
                         activation='elu', input_shape=[28, 28, 1]),
@@ -22,10 +26,14 @@ model = keras.models.Sequential([
     keras.layers.Dense(10, activation="softmax")
 ])
 
+# compiling the model
 model.compile(loss="sparse_categorical_crossentropy", metrics=[
               "accuracy"], optimizer=keras.optimizers.SGD(lr=0.08))
 print(model.summary())
+# train the model
 model.fit(x_train, y_train, epochs=5)
+# Evaluate the model
 model.evaluate(x_test, y_test)
+# saving the model
 model.save('model.h5')
 print('Model Saved')
